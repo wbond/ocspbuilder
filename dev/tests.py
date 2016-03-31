ef69b69f78d5c1d0a3714f66d32475f7463d5385
wbond <will@wbond.net>
2016-03-31 05:21:12 -0400

    Make "setup.py test" work [ci skip]

--- dev/tests.py
+++ dev/tests.py
@@ -11,6 +11,23 @@ from tests.test_ocsp_request_builder import OCSPRequestBuilderTests
 test_classes = [OCSPResponseBuilderTests, OCSPRequestBuilderTests]
 
 
+def make_suite():
+    """
+    Constructs a unittest.TestSuite() of all tests for the package. For use
+    with setuptools.
+
+    :return:
+        A unittest.TestSuite() object
+    """
+
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    for test_class in test_classes:
+        tests = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(tests)
+    return suite
+
+
 def run(matcher=None):
     """
     Runs the tests
